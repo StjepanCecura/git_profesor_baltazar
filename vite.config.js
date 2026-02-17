22c055ad96bbb155be454f45102dfe6155022890
Stjepan Cecura <stjepan.cecura777@gmail.com>
2026-02-17 15:41:12 +0100

    Popravio vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 
 export default defineConfig({
+  root: 'src',
+  publicDir: '../public',
   base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@engine': path.resolve(__dirname, 'src/engine'),
+      '@scenes': path.resolve(__dirname, 'src/scenes'),
+      '@task-file': path.resolve(__dirname, 'src/task-file')
+    }
   }
 })
