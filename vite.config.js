f602d4b3c6b095c9152dea87accfc07b4bbc450f
Stjepan Cecura <stjepan.cecura777@gmail.com>
2026-02-17 15:04:16 +0100

    Dodani environment files i vite.config promijenjen

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
+  base: '/',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@engine': path.resolve(__dirname, 'src/engine'),
-      '@scenes': path.resolve(__dirname, 'src/scenes'),
-      '@task-file': path.resolve(__dirname, 'src/task-file'),
-    }
-  },
-  server: {
-    open: true,
-    port: 3000,
-    host: '0.0.0.0' // Allow access from local network (safe - only accessible on your WiFi)
+    outDir: 'dist',
+    emptyOutDir: true
   }
-});
+})
